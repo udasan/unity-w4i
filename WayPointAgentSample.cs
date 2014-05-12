13289fac67aa59b39996925ee9d2a977c01f2a1a
Udasan <udasan.koubou@gmail.com>
2014-05-13 00:43:52 +0900

    add a sample script

---
+++ WayPointAgentSample.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WayPointAgentSample : MonoBehaviour
+{
+	public string tweenName;
+	public string pathName;
+	public bool moveToPath = true;
+	public float speed = 1.0f;
+	public iTween.EaseType easeType = iTween.EaseType.linear;
+
+	void Start ()
+	{
+		iTween.MoveTo(this.gameObject, iTween.Hash(
+			"name", tweenName,
+			"path", WayPointPath.Get(pathName).path,
+			"movetopath", moveToPath,
+			"speed", speed,
+			"easetype", easeType));
+	}
+}
