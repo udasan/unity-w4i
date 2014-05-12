0d3163ae35dc4e183b545305b824133531501f14
Udasan <udasan.koubou@gmail.com>
2014-05-13 00:43:32 +0900

    add scripts

---
+++ WayPoint.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WayPoint : MonoBehaviour
+{
+	public WayPoint next;
+
+	void OnDrawGizmosSelected ()
+	{
+		WayPointPath wayPointPath = null;
+		Transform target = this.transform.parent;
+		while (target && !(wayPointPath = target.GetComponent<WayPointPath>())) { target = target.parent; }
+
+		if (wayPointPath && (System.Array.IndexOf(wayPointPath.wayPoints, this) >= 0)) {
+			wayPointPath.OnDrawGizmosSelected();
+		}
+	}
+}
---
+++ WayPointPath.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WayPointPath : MonoBehaviour
+{
+	public static Dictionary<string, WayPointPath> paths = new Dictionary<string, WayPointPath>();
+
+	public static WayPointPath Get (string pathName)
+	{
+		WayPointPath wayPointPath;
+		paths.TryGetValue(pathName, out wayPointPath);
+		return wayPointPath;
+	}
+
+	public string pathName;
+	public WayPoint head;
+
+	public Color pathColor = Color.cyan;
+
+	public WayPoint[] wayPoints {
+		get {
+			List<WayPoint> nodes = new List<WayPoint>();
+			WayPoint node = head;
+			while (node) {
+				nodes.Add(node);
+				node = node.next;
+			}
+			return nodes.ToArray();
+		}
+	}
+
+	public Vector3[] path {
+		get {
+			return System.Array.ConvertAll(wayPoints, x => x.transform.position);
+		}
+	}
+
+	void OnEnable ()
+	{
+		if (!paths.ContainsKey(pathName)) {
+			paths.Add(pathName, this);
+		}
+	}
+	
+	void OnDisable ()
+	{
+		paths.Remove(pathName);
+	}
+
+	public void OnDrawGizmosSelected ()
+	{
+		if (head && head.next) { // path.Length >= 2
+			iTween.DrawPath(path, pathColor);
+		}
+	}
+}
